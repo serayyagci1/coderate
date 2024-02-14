da86bdd59e3dac486ca993ac928e3045a88d8dc0
ybo-source <74118699+ybo-source@users.noreply.github.com>
2024-02-14 18:01:26 +0100

    Add files via upload

---
+++ data_cleaner_2022.py
@@ -0,0 +1,73 @@
+
+# This code cleans the Raw Dataset and makes it ML model-suitable Dataset.
+import pandas as pd
+# Read the Data set.
+# Drop the duplicate and the NA value containing rows.
+def data_clearer(file):
+    df = pd.read_csv(file)
+    is_null = df.isnull().values.any()
+    if is_null:
+        df.dropna(inplace=True)
+    df.drop_duplicates(inplace=True)
+    return (df)
+
+# Read the csv file with the data_clearer function.
+df = data_clearer("/Users/yusufberkoruc/PycharmProjects/heart_disease_predictor/heart_2022_no_nans.csv")
+
+# Merge the Heart Attack and the Angina columns to create single target variable "HeartDisease".
+def disease_maker(row):
+    if row['HadHeartAttack'] == "Yes" or row['HadAngina'] == "Yes":
+        return "Yes"
+    else:
+        return "No"
+df['HeartDisease'] = df.apply(disease_maker, axis=1)
+
+#After Creating a new disease column drop the old columns.
+df.drop(['HadHeartAttack', 'HadAngina',"RemovedTeeth"],axis =1,inplace =True)
+
+# Use the clearer function to transform data into the ML model suited form.
+def diabetes_clearer(element):
+    dict_temp ={'No':"No", 'Yes':"Yes", 'No, pre-diabetes or borderline diabetes':"No", 'Yes, but only during pregnancy (female)':"Yes"}
+    return dict_temp[element]
+df["HadDiabetes"] = df['HadDiabetes'].apply(diabetes_clearer)
+
+# Use the clearer function to transform data into the ML model suited form.
+def smoker_clearer(element):
+    dict_temp ={'Current smoker - now smokes every day':"Yes", 'Former smoker':"Yes", 'Never smoked':"No", 'Current smoker - now smokes some days':"Yes"}
+    return dict_temp[element]
+df["SmokerStatus"] = df['SmokerStatus'].apply(smoker_clearer)
+# Use the clearer function to transform data into the ML model suited form.
+def eCigarete_clearer(element):
+    dict_temp ={'Not at all (right now)':"No", 'Use them some days':"Yes", 'Use them every day':"Yes", 'Never used e-cigarettes in my entire life':"No"}
+    return dict_temp[element]
+df["ECigaretteUsage"] = df['ECigaretteUsage'].apply(eCigarete_clearer)
+
+# Use the clearer function to transform data into the ML model suited form.
+def RaceEthnicityCategory_clearer(element):
+    dict_temp ={'Hispanic':"Hispanic", 'Other race only, Non-Hispanic':"Other", 'Multiracial, Non-Hispanic':"Multiracial", 'White only, Non-Hispanic':"White", 'Black only, Non-Hispanic':"Black"}
+    return dict_temp[element]
+df["RaceEthnicityCategory"] = df['RaceEthnicityCategory'].apply(RaceEthnicityCategory_clearer)
+
+# Use the clearer function to transform data into the ML model suited form.
+def TetanusLast10Tdap_clearer(element):
+   return  element.split(",")[0]
+df["TetanusLast10Tdap"] = df['TetanusLast10Tdap'].apply(TetanusLast10Tdap_clearer)
+
+# Use the clearer function to transform data into the ML model suited form.
+def CovidPos_clearer(element):
+    dict_temp = {'No':"No", 'Tested positive using home test without a health professional':"Yes", 'Yes':"Yes"}
+    return dict_temp[element]
+df["CovidPos"] = df['CovidPos'].apply(CovidPos_clearer)
+
+
+# Drop the redundant columns which are captured in the BMI column.
+df.drop(['HeightInMeters', 'WeightInKilograms'],axis =1,inplace =True)
+
+#Save the Cleaned Data.
+df.to_csv("heart_2022_cleared.csv",index=False)
+
+#Check the features of the Clean Data.
+df.info()
+print(df.nunique())
+
+
---
+++ data_merge_2020_2022.py
@@ -0,0 +1,77 @@
+#This code cleans the 2.nd Raw Dataset and makes it ML model-suitable Dataset.
+#This code merges 2 Datasets and samples the final Dataset to create a balanced dataset for the ML training.
+import pandas as pd
+
+# Read the Data set.
+# Drop the duplicate and the NA value containing rows.
+def data_clearer(file):
+    df = pd.read_csv(file)
+    is_null = df.isnull().values.any()
+    if is_null:
+        df.dropna(inplace=True)
+    df.drop_duplicates(inplace=True)
+    return (df)
+# Standardize the age values between the datasets.
+def age_closer(element):
+    if element.startswith("Age"):
+        if "or" in element:
+            element = '80 or older'
+        else:
+            element = "-".join(element.split(" ")[1:4:2])
+    return element
+# Use the clearer function to transform data into the ML model suited form.
+def diabetes_clearer(element):
+    dict_temp ={'No':"No", 'Yes':"Yes", 'No, borderline diabetes':"No", 'Yes (during pregnancy)':"Yes"}
+    return dict_temp[element]
+
+
+# Read the csv files with the data_clearer function.
+df_2020 = data_clearer("/Users/yusufberkoruc/PycharmProjects/heart_disease_predictor/heart_2020_cleaned.csv")
+df_2022 = pd.read_csv("/Users/yusufberkoruc/PycharmProjects/heart_disease_predictor/heart_2022_cleared.csv")
+
+#Apply the diabetes_clearer function.
+df_2020["Diabetic"] = df_2020['Diabetic'].apply(diabetes_clearer)
+
+# Fİnd the column names to standardize column names between two datasets.
+list_2020_columns = list(df_2020.columns)
+list_2022_columns = list(df_2022.columns)
+
+# Standardize the column names.
+rename_dict = dict()
+for i in list_2020_columns:
+    for k in list_2022_columns:
+        if i in k:
+            rename_dict[k] = i
+#Final Dictionary for the column name standardization.
+dict_rename = {'HadDiabetes':'Diabetic','PhysicalActivities':'PhysicalActivity','SleepHours':'SleepTime','DifficultyWalking':'DiffWalking','GeneralHealth':'GenHealth','AlcoholDrinkers':'AlcoholDrinking','SmokerStatus':'Smoking','HeartDisease': 'HeartDisease', 'BMI': 'BMI', 'HadStroke': 'Stroke', 'PhysicalHealthDays': 'PhysicalHealth', 'MentalHealthDays': 'MentalHealth', 'Sex': 'Sex', 'AgeCategory': 'AgeCategory', 'RaceEthnicityCategory': 'Race', 'HadAsthma': 'Asthma', 'HadKidneyDisease': 'KidneyDisease', 'HadSkinCancer': 'SkinCancer'}
+
+#Change the column names.
+df_2022 = df_2022.rename(columns=dict_rename)
+
+#Subset the 2022 data to have same columns.
+df_2022 = df_2022[dict_rename.values()]
+
+#Merge the Datasets.
+concat_df = pd.concat([df_2020,df_2022], ignore_index=True)
+
+# Apply the age_closer function
+concat_df["AgeCategory"] = concat_df['AgeCategory'].apply(age_closer)
+
+# Split the Dataset as the positive and negative.
+concat_df_positive = concat_df[concat_df['HeartDisease'] == "Yes" ]
+concat_df_negative = concat_df[concat_df['HeartDisease'] == "No" ]
+
+# Sample the negative dataset to evade bias.
+sampled_df = concat_df.sample(len(concat_df_positive),random_state=42)
+
+#Merge the sampled negative dataset and the positive data set.
+result_df = pd.concat([sampled_df , concat_df_positive], ignore_index=True)
+
+#Save the final dataset to be used for the ML training.
+result_df.to_csv("final_evaluation_data.csv",index=False)
+
+#Check the features of the Final Dataset.
+result_df.info()
+print(result_df.nunique())
+
+
