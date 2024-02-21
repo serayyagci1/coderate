53b7c6cd350624cf12521f317608d5d0c780b932
serayyagci1 <150111887+serayyagci1@users.noreply.github.com>
2024-02-21 05:59:24 +0300

    Added additional comments

--- heartProject - Django project/heartProjectApp/forms.py
+++ heartProject - Django project/heartProjectApp/forms.py
@@ -1,24 +1,30 @@
 from django import forms
 from django.forms import Select
 from django.utils.safestring import mark_safe
-
+#The ExtendedInfoWidget class defined which extends the Django 'Select' form widget
 class ExtendedInfoWidget(Select):
+    #Constructor of this class, takes info_text and other arguments
     def __init__(self, info_text, *args, **kwargs):
+        #initializes info_text as popover if passed through form element
         self.info_text = info_text
+        #calls init method for initialization
         super().__init__(*args, **kwargs)
-
+    #Overrides default 'Selectt' initialization, 
     def render(self, name, value, attrs=None, renderer=None):
+        #Renders widget using parent render, stores rendered html in rendered_widget
         rendered_widget = super().render(name, value, attrs, renderer)
+        #Populates the rendered widget element with a button that displays the info_text, and does it with
+        #mark_safe to ensure django that string is safe for HTML rendering
         return mark_safe(rendered_widget + f'<button type="button" class="info-button" data-toggle="popover" data-content="{self.info_text}"><img src="../static/heartProjectApp/home-images/info.svg"></button>')
-
+#Creating the HeartDiseaseForm class
 class HeartDiseaseForm(forms.Form):
-   #Creating the forms elements and specifying what kind of values the user can put in
+   #Creating the forms elements and specifying what kind of values are allowed
     BMI = forms.ChoiceField(
         choices=[(i, str(i)) for i in range(1, 101)],
         label='BMI',
     
     )
-
+    #ExtendedInfoWidget is added to these two elements because they need extra information, info_text is displayed as a popover text
     PhysicalHealth = forms.ChoiceField(
         choices=[(i, str(i)) for i in range(1, 32)],
         label='Physical Health',
@@ -151,7 +157,7 @@ class HeartDiseaseForm(forms.Form):
         for field_name in ['BMI', 'PhysicalHealth', 'MentalHealth']:
             self.fields[field_name].widget.attrs['class'] = 'form-control'
 
-        # Set default values for selected fields
+        # Set default values for specific fields that would thought to allow for faster completion
         self.fields['BMI'].initial = 25
         self.fields['PhysicalHealth'].initial = 15
         self.fields['MentalHealth'].initial = 15
